b33529d012d06cef79c3dcb89e7a76927f63d261
Nishan Bhattrai <nishan.bhattrai@aitcentre.com>
2024-08-12 14:28:21 +0545

    small changes

--- components/user/home/ProjectCard.tsx
+++ components/user/home/ProjectCard.tsx
@@ -2,25 +2,25 @@
 import React, { useRef } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { projectCategoryType } from "@/utils/types/dto";
-const ProjectCard = ({ category }:{category:projectCategoryType}) => {
-  const lotteRef = useRef();
+const ProjectCard = ({ category }: { category: projectCategoryType }) => {
+  const lotteRef = useRef<Player | null>(null);
   return (
     <div className="project-card relative cursor-pointer border h-80 shadow-custom w-full gap-4 p-4 justify-center bg-gray-50 rounded-xl flex flex-col">
-    <Player
-      ref={lotteRef}
-      autoplay={true}
-      loop={true}
-      controls={true}
-       src={"/lotties/animationweb.json"}
-      style={{ height: "80px", width: "80px" }}
-    ></Player>
-    <h2 className="font-medium text-[#003366] text-center text-xl">
-      {category?.name}
-    </h2>
-    <p className="text-sm text-[#222222] text-center">
-      {category.description}
-    </p>
-  </div>
+      <Player
+        ref={lotteRef}
+        autoplay={true}
+        loop={true}
+        controls={true}
+        src={"/lotties/animationweb.json"}
+        style={{ height: "80px", width: "80px" }}
+      ></Player>
+      <h2 className="font-medium text-[#003366] text-center text-xl">
+        {category?.name}
+      </h2>
+      <p className="text-sm text-[#222222] text-center">
+        {category.description}
+      </p>
+    </div>
   );
 };
 
--- components/user/home/ProjectSection.tsx
+++ components/user/home/ProjectSection.tsx
@@ -1,31 +1,30 @@
-import { TopDownAnimation } from './../../common/animation/TopDownAnimaton'
-import { projectCategory } from '@/utils/deleteable/project-section'
-import ProjectCard   from "./ProjectCard";
-
+import { TopDownAnimation } from "./../../common/animation/TopDownAnimaton";
+import { projectCategory } from "@/utils/deleteable/project-section";
+import ProjectCard from "./ProjectCard";
+import { projectCategoryType } from "@/utils/types/dto";
 
 const ProjectSection = () => {
-  const ={eded}
   return (
     <section className="project-section bg-secondary py-10 sm:py-20 px-4 sm:px-10  w-full">
-    <TopDownAnimation>
-    <div className="pb-10">
-          
-    <h2 className="section-title text-center text-[48px] tracking-wide  text-highlight font-semibold">
-      My Expertise
-    </h2>
-    <p className="text-base text-center text-[#003366]">
-      Discover my range of professional services designed to help you
-      achieve your business goals.
-    </p>
-  </div></TopDownAnimation>
-       
-        <div className="project-card-wrapper grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-          {projectCategory.map((category: projectCategoryType) => (
-            <ProjectCard key={category.id} category={category} />
-          ))}
+      <TopDownAnimation>
+        <div className="pb-10">
+          <h2 className="section-title text-center text-[48px] tracking-wide  text-highlight font-semibold">
+            My Expertise
+          </h2>
+          <p className="text-base text-center text-[#003366]">
+            Discover my range of professional services designed to help you
+            achieve your business goals.
+          </p>
         </div>
-      </section>
-  )
-}
+      </TopDownAnimation>
 
-export default ProjectSection
+      <div className="project-card-wrapper grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+        {projectCategory.map((category: projectCategoryType) => (
+          <ProjectCard key={category.id} category={category} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectSection;
