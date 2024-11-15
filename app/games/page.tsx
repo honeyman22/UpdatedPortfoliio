b5297201d312188edd4d16006ddfb6b3e01afabc
Nishan <nishan.bhattrai@aitcentre.com>
2024-11-15 15:15:26 +0545

    feat:games

---
+++ app/games/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div className="h-[200vh] w-full bg-green-600">hello</div>;
+};
+
+export default Page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <div className="mt-[112px]"></div>
         {children}
       </body>
     </html>
